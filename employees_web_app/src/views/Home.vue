94132a880e1547da2756be5f6855e2ade8bad1b1
Pili Sánchez <sanchezpili6@gmail.com>
2021-10-17 18:01:15 -0500

    Vue and Vuetify installation

--- employees_web_app/src/views/Home.vue
+++ employees_web_app/src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-row>
+      <v-col cols="4">
+        <v-card>
+          <v-card-title>Tabla de empleados</v-card-title>
+          <v-card-text>
+            <v-data-table>
+
+            </v-data-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Detalle empleados</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+    },
   }
-}
 </script>
